372aeb293fe1e2851fc3d6e5784434a0070b1336
Niña Margarette <marga18nins@gmail.com>
2024-10-02 11:41:37 +0800

    navbar fixed

--- PawsForHome/main/urls.py
+++ PawsForHome/main/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('browse/', views.browse, name='browse'),
-    path('notification/', views.notifications, name='notifications'),
+    path('notifications/', views.notifications, name='notifications'),
     path('message/', views.message, name='message'),
     path('profile/', views.profile, name='profile'),
     path('signup/', views.create_account, name='create_account'),
