27b00c916ba344e3001a53c0ecfa42fb59941798
kyahki <genjikyle123@gmail.com>
2024-10-02 13:25:25 +0800

    Applied Login & Landing Page Functions

--- PawsForHome/main/urls.py
+++ PawsForHome/main/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('',views.landing_page,name='landing_page'),
+    path('index/', views.index, name='index'),
     path('notifications/', views.notifications, name='notifications'),
     path('message/', views.message, name='message'),
     path('profile/', views.profile, name='profile'),
     path('signup/', views.create_account, name='create_account'),
+    path('login/',views.login_account, name='login_account'),    
+    path('home/',views.home, name='home'),    
 ]
--- PawsForHome/main/views.py
+++ PawsForHome/main/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password
 from django.contrib import messages
 from .models import Account
-from .forms import Create_Account
+from .forms import Create_Account, Login_Account
+from django.contrib.auth import login
 
 # Create your views here.
+
+def landing_page(request):
+     return render(request, 'landing_page.html', {})
 def index(request):
     return render(request, 'index.html', {})
 
@@ -16,6 +20,9 @@ def message(request):
 
 def profile(request):
     return render(request, 'navbar/profile.html', {})
+
+def home(request):
+    return render(request, 'home.html', {})
 
 def create_account(request):
     accounts = Account.objects.all()
@@ -35,7 +42,8 @@ def create_account(request):
                 hash_password = make_password(password=password)
                 create_account = Account(account_type=account_type, email=email, password=hash_password)
                 create_account.save()
-                return render(request,'index.html', {})
+                # return render(request,'index.html', {})
+                return redirect('main:login_account')
                 
             elif confirm_password!=password: 
                 # print("password is not the same")
@@ -47,3 +55,30 @@ def create_account(request):
         form = Create_Account()
 
     return render(request, 'login_signup/signup.html', {'form':form})
+
+def login_account(request):
+    if request.method == 'POST':
+        form = Login_Account(request.POST)
+        
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            # user = authenticate(request, username=email, password=password)
+            if Account.objects.filter(email=email).exists():
+                login_account = Account.objects.get(email=email)
+                if login_account.check_password(password):  
+                    # login(request, login_account)  
+                    return redirect('main:home')  
+                else:
+                    error_message = "Invalid password."
+            else:
+                error_message = "Account with this email does not exist."
+        #     if user:
+        #         login(request, user)
+        #         return redirect('home')
+        # error_message = "Invalid email or password."
+        return render(request, 'login_signup/login.html', {'form': form, 'error_message': error_message})
+
+    else:  
+        form = Login_Account()
+        return render(request, 'login_signup/login.html', {'form': form})
