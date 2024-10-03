f64f2993296b05964f0275ed90ebdfa8ee8a0d49
Niña Margarette <marga18nins@gmail.com>
2024-10-03 09:47:23 +0800

    some small changes in AccountManager class

--- PawsForHome/main/models.py
+++ PawsForHome/main/models.py
@@ -6,6 +6,8 @@ from django.contrib.auth.hashers import make_password, check_password
 
 class AccountManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email is required')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -15,6 +17,11 @@ class AccountManager(BaseUserManager):
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
         return self.create_user(email, password, **extra_fields)
     
 class Account(AbstractBaseUser, PermissionsMixin):
